9ce7380b9befe659fa0f73d66b16bd17e695791e
jthiel <lthiel@dsoftworks.de>
2025-01-12 19:23:21 +0100

    Preise von Strings zu Numbers umgewandelt

--- script.js
+++ script.js
@@ -2,29 +2,39 @@ let myMenu = [
     {
      "courts": "Pizza Salami",
      "with": "Salami und Käse",
-     "price": "8,50 €"
+     "price": 8.50
      },  
      {
      "courts": "Pizza Schinken",
      "with": "Schinken und Käse",
-     "price": "8,50 €"
+     "price": 8.50
      },
      {
      "courts": "Spaghethi Bolognese",
      "with": "Hackfleisch, Gemüse und Sause",
-     "price": "9,50 €"
+     "price": 9.50
     },
     {
       "courts": "Spaghethi Carbonara",
       "with": "Schinken und Käsesahnesoße",
-      "price": "9,50 €"
+      "price": 9.50
      },
      {
       "courts": "Dönerteller mit Halloumi",
       "with": "mit Pommes und Hawaiisauce",
-      "price": "11,00 €"
+      "price": 11.00
      }
  ];
+ 
+//  let cart = [];
+
+//  let menu = document.getElementById('content');
+//  myMenu.contentRef = menu {
+
+//    for (let indexMyMenu = 0; indexMyMenu < array.length; indexMyMenu++) {
+//       const element = array[indexMyMenu];  
+//    }
+//  };
  
  function renderMyMenu() {
     let contentRef = document.getElementById('content');
@@ -38,22 +48,22 @@ let myDessert = [
     {
      "courts": "Donat",
      "with": "Schoko und Vanillefüllung",
-     "price": "5,50 €"
+     "price": 5.50
      },  
      {
      "courts": "Käsekunchen",
      "with": "Erdbeer Sahnefüllung",
-     "price": "6,50 €"
+     "price": 6.50
      },
      {
      "courts": "Vanilleeis",
      "with": "heißen Himmbeeren",
-     "price": "5,50 €"
+     "price": 5.50
     },
     {
       "courts": "Pfannenkuchen",
       "with": "Erdbeermarmelade",
-      "price": "4,50 €"
+      "price": 4.50
      }
  ];
 
@@ -69,22 +79,22 @@ let myDrinks = [
     {
      "courts": "Coca Cola zero",
      "with": "whitout Sugar",
-     "price": "5,50 €"
+     "price": 5.50
      },
      {
       "courts": "Eistee",
       "with": "Pfirsichgeswchmack",
-      "price": "5,50 €"
+      "price": 5.50
       }, 
      {
      "courts": "Orangensaft",
      "with": "Frischen Orangen",
-     "price": "6,50 €"
+     "price": 6.50
      },
      {
      "courts": "Kaffee",
      "with": "Milch und Zucker",
-     "price": "5,50 €"
+     "price": 5.50
     }
  ];
 
