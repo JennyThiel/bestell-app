7ba9f47c69ec0971da112254fa565b2e60c539a1
jthiel <lthiel@dsoftworks.de>
2025-01-18 17:33:10 +0100

    Script -> Renderfunktion hinzugefügt ->addfunktion hinzugefügt ->saveto LocalStoige hinzugefügt

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
 
- function renderMyMenu() {
-    let contentRef = document.getElementById('content');
+function renderMyMenu() {
+   let menuContentRef = document.getElementById('content');
    
-    for (let indexMyMenu = 0; indexMyMenu < myMenu.length; indexMyMenu++) {
-       contentRef.innerHTML += getNoteTemplateMenu(indexMyMenu);
-    }
+   for (let indexMyMenu = 0; indexMyMenu < myMenu.length; indexMyMenu++) {
+      menuContentRef.innerHTML += getNoteTemplateMenu(indexMyMenu);
+   }
 }
 
- function renderMyDessert() {
-    let contentRef = document.getElementById('dessertContent');
+function renderMyDessert() {
+   let dessertContentRef = document.getElementById('dessertContent');
    
-    for (let indexMyDessert = 0; indexMyDessert < myDessert.length; indexMyDessert++) {
-       contentRef.innerHTML += getNoteTemplateDessert(indexMyDessert);
-    }
+   for (let indexMyDessert = 0; indexMyDessert < myDessert.length; indexMyDessert++) {
+      dessertContentRef.innerHTML += getNoteTemplateDessert(indexMyDessert);
+   }
 }
 
  function renderMyDrinks() {
-    let contentRef = document.getElementById('drinksContent');
+   let drinksContentRef = document.getElementById('drinksContent');
    
-    for (let indexMyDrinks = 0; indexMyDrinks < myDrinks.length; indexMyDrinks++) {
-       contentRef.innerHTML += getNoteTemplateDrinks(indexMyDrinks);
-    }
+   for (let indexMyDrinks = 0; indexMyDrinks < myDrinks.length; indexMyDrinks++) {
+      drinksContentRef.innerHTML += getNoteTemplateDrinks(indexMyDrinks);
+   }
 }
+
+function renderMyOrder() {
+   let cartContentRef = document.getElementById('orderbasket');
+
+   for (let indexCart = 0; indexCart < cart.length; indexCart++) {
+      cartContentRef.innerHTML += getNoteTemplateOrderCart(indexCart)
+   }
+}
+
+function addToCart() {
+   let myMenuRef = document.getElementById('myMenuBtn');
+
+   return myMenu.find(menu => myMenu.id === id);
+}
+
+let sellectedProduct = addToCart(2);
+
+
+function saveToLocalStorage() {
+   localStorage.setItem("myMenu", JSON.stringify(myMenu));
+   localStorage.setItem("myDessert", JSON.stringify(myDessert));
+   localStorage.setItem("myDrinks", JSON.stringify(myDrinks));
+   localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+// Lädt die Daten aus dem Local Storage oder erstellt leere Arrays
+function getFromLocalStorage() {
+   myMenu = JSON.parse(localStorage.getItem("myMenu"));
+   myDessert = JSON.parse(localStorage.getItem("myDessert"));
+   myMenu = JSON.parse(localStorage.getItem("myDrinks"));
+   cart = JSON.parse(localStorage.getItem("cart"));
+}
+
